26f2462a650cf2348f295d2a26ffd2fce8017e89
agmercier <arthur.g.mercier@gmail.com>
2024-06-08 18:08:07 +0200

    Fixed variation only using half of the parents

--- vangogh/variation.py
+++ vangogh/variation.py
@@ -5,73 +5,76 @@ from vangogh.util import NUM_VARIABLES_PER_POINT
 
 
 def crossover(genes, method="ONE_POINT"):
-    parents_1 = np.vstack((genes[:len(genes) // 2], genes[:len(genes) // 2]))
-    parents_2 = np.vstack((genes[len(genes) // 2:], genes[len(genes) // 2:]))
+    parents_1 =genes[:len(genes) // 2]
+    parents_2 = genes[len(genes) // 2:]
     offspring = np.zeros(shape=genes.shape, dtype=int)
-
+    
+    
     if method == "ONE_POINT":
-        crossover_points = np.random.randint(0, genes.shape[1], size=genes.shape[0])
-        for i in range(len(genes)):
-            offspring[i,:] = np.where(np.arange(genes.shape[1]) <= crossover_points[i], parents_1[i,:], parents_2[i,:])
+        crossover_points = np.random.randint(0, genes.shape[1], size=parents_1.shape[0])
+        for i in range(len(parents_1)):
+            offspring[2*i,:] = np.where(np.arange(genes.shape[1]) <= crossover_points[i], parents_1[i,:], parents_2[i,:])
+            offspring[2*i+1,:] = np.where(np.arange(genes.shape[1]) <= crossover_points[i], parents_2[i,:],parents_1[i,:])
     
     # Uniform Crossover
     elif method == "UNIFORM":
-        for i in range(0, len(genes), 2):
+        for i in range(len(parents_1)):
             off_1, off_2 = uniform_crossover(parents_1[i,:], parents_2[i,:])
-            offspring[i, :] = off_1
-            offspring[i+1,:] = off_2
+            offspring[2*i, :] = off_1
+            offspring[2*i+1,:] = off_2
             
     elif method == "UNIFORM_DISTINCT":
-        print("rrr")
-        for i in range(0, len(genes), 2):
+        for i in range(len(parents_1)):
             off_1, off_2 = uniform_crossover_distinct(parents_1[i,:], parents_2[i,:])
-            offspring[i, :] = off_1
-            offspring[i+1,:] = off_2
+            offspring[2*i, :] = off_1
+            offspring[2*i+1,:] = off_2
 
     # k-points
-    elif method[0].isdigit() and method[1:] == '_POINT':
-        num_points = int(list(filter(str.isdigit, method))[0])
+    elif method[0].isdigit() and method.endswith("_POINT"):
+        num_splits = int(list(filter(str.isdigit, method))[0])
         length = len(parents_1)
-        crossover_points = np.sort(np.random.randint(0, genes.shape[1], size=(genes.shape[0], num_points)))
+        crossover_points = np.sort(np.random.randint(0, genes.shape[1], size=(length, num_splits)))
         
-        offspring = parents_1.copy()
 
-        for i in range(0, len(genes), 2):
+        for i in range(length):
             for j in range(crossover_points.shape[1]):
                 start = crossover_points[i,j]
                 end = length
                 if j + 1 < crossover_points.shape[1]:
                     end = crossover_points[i,j+1]
                 if j % 2 == 0:
-                    offspring[i, start:end] = parents_2[i, start:end]
+                    offspring[2*i, start:end] = parents_2[i, start:end]
+                    offspring[2*i+1, start:end] = parents_1[i, start:end]
                 else:
-                    offspring[i+1, start:end] = parents_2[i, start:end]
-    elif method[0].isdigit() and method[1:] == '_POINT_DISTINCT':
-        num_points = int(list(filter(str.isdigit, method))[0])
+                    offspring[2*i+1, start:end] = parents_2[i, start:end]
+                    offspring[2*i, start:end] = parents_1[i, start:end]
+    elif method[0].isdigit() and method.endswith("_POINT_DISTINCT"):
+        num_splits = int(list(filter(str.isdigit, method))[0])
         length = len(parents_1)
-        crossover_points = np.sort(np.random.randint(0, int(genes.shape[1] // NUM_VARIABLES_PER_POINT), size=(genes.shape[0], num_points)))
+        crossover_points = np.sort(np.random.randint(0, int(genes.shape[1] // NUM_VARIABLES_PER_POINT), size=(length, num_splits)))
         crossover_points = crossover_points * NUM_VARIABLES_PER_POINT
-        offspring = parents_1.copy()
 
-        for i in range(0, len(genes), 2):
+        for i in range(length):
             for j in range(crossover_points.shape[1]):
                 start = crossover_points[i,j]
                 end = length
                 if j + 1 < crossover_points.shape[1]:
                     end = crossover_points[i,j+1]
                 if j % 2 == 0:
-                    offspring[i, start:end] = parents_2[i, start:end]
+                    offspring[2*i, start:end] = parents_2[i, start:end]
+                    offspring[2*i+1, start:end] = parents_1[i, start:end]
                 else:
-                    offspring[i+1, start:end] = parents_2[i, start:end]
+                    offspring[2*i+1, start:end] = parents_2[i, start:end]
+                    offspring[2*i, start:end] = parents_1[i, start:end]
+                    
     #space
-    elif method[0].isdigit() and method[1:] == '_SPATIAL':
-        offspring = np.zeros(shape=genes.shape, dtype=int)
-        for i in range(0,len(genes),2):
-
+    elif method[0].isdigit() and method.endswith("_SPATIAL"):
+        num_splits = int(list(filter(str.isdigit, method))[0])
+        for i in range(len(parents_1)):
             #reshape as points
             parent_1_points = []
             parent_2_points = []
-            num_points = int(len(genes[i]) / NUM_VARIABLES_PER_POINT)
+            num_points = int(len(parents_1[i]) / NUM_VARIABLES_PER_POINT)
             for r in range(num_points):
                 p = r * NUM_VARIABLES_PER_POINT
                 x, y, r, g, b = parents_1[i,p:p + NUM_VARIABLES_PER_POINT]
@@ -80,19 +83,18 @@ def crossover(genes, method="ONE_POINT"):
                 x, y, r, g, b = parents_2[i,p:p + NUM_VARIABLES_PER_POINT]
                 parent_2_points.append((x , y,r,g,b))
             
-            offspring_points_1, offspring_points_2 = split_parents(parent_1_points, parent_2_points, int(method[0]), mutate=True)
+            offspring_points_1, offspring_points_2 = split_parents(parent_1_points, parent_2_points, num_splits, mutate=True)
 
             #flatten
-            offspring[i, :] = np.concatenate(offspring_points_1).ravel().tolist()
-            offspring[i+1, :] = np.concatenate(offspring_points_2).ravel().tolist()
-    elif method[0].isdigit() and method[1:] == '_SPATIAL_DISTINCT':
-        offspring = np.zeros(shape=genes.shape, dtype=int)
-        for i in range(0,len(genes),2):
-
+            offspring[2*i, :] = np.concatenate(offspring_points_1).ravel().tolist()
+            offspring[2*i+1, :] = np.concatenate(offspring_points_2).ravel().tolist()
+    elif method[0].isdigit() and method.endswith("_SPATIAL_DISTINCT"):
+        num_splits = int(list(filter(str.isdigit, method))[0])
+        for i in range(len(parents_1)):
             #reshape as points
             parent_1_points = []
             parent_2_points = []
-            num_points = int(len(genes[i]) / NUM_VARIABLES_PER_POINT)
+            num_points = int(len(parents_1[i]) / NUM_VARIABLES_PER_POINT)
             for r in range(num_points):
                 p = r * NUM_VARIABLES_PER_POINT
                 x, y, r, g, b = parents_1[i,p:p + NUM_VARIABLES_PER_POINT]
@@ -101,13 +103,13 @@ def crossover(genes, method="ONE_POINT"):
                 x, y, r, g, b = parents_2[i,p:p + NUM_VARIABLES_PER_POINT]
                 parent_2_points.append([x , y,r,g,b])
             
-            offspring_points_1, offspring_points_2 = split_parents(parent_1_points, parent_2_points, int(method[0]))
+            offspring_points_1, offspring_points_2 = split_parents(parent_1_points, parent_2_points, num_splits)
 
             #flatten
-            offspring[i, :] = np.concatenate(offspring_points_1).ravel().tolist()
-            offspring[i+1, :] = np.concatenate(offspring_points_2).ravel().tolist()
+            offspring[2*i, :] = np.concatenate(offspring_points_1).ravel().tolist()
+            offspring[2*i+1, :] = np.concatenate(offspring_points_2).ravel().tolist()
     else:
-        raise Exception("Unknown crossover method")
+        raise Exception("Unknown crossover method", method)
 
     return offspring
 
@@ -131,8 +133,8 @@ def split_parents(parent_1_points, parent_2_points, split,  mutate=False):
         else:
             return np.concatenate([parent_1_points[:int(length//2)], parent_2_points[int(length//2):]]), np.concatenate([parent_2_points[:int(length//2)], parent_1_points[int(length//2):]])
 
-    left_side_1, left_side_2 = split_parents(parent_1_points[:int(length//2)], parent_2_points[:int(length//2)], split-1)
-    right_side_1, right_side_2 = split_parents(parent_1_points[int(length//2):], parent_2_points[int(length//2):], split-1)
+    left_side_1, left_side_2 = split_parents(parent_1_points[:int(length//2)], parent_2_points[:int(length//2)], split=split-1)
+    right_side_1, right_side_2 = split_parents(parent_1_points[int(length//2):], parent_2_points[int(length//2):], split=split-1)
 
     if mutate:
         return merge_points(left_side_1, right_side_1), merge_points(left_side_2, right_side_2)
